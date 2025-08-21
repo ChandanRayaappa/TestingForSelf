f7f6df520f707a500914974b272b96c831fb5d76
Chandankumar <chandanraayappab@gmail.com>
2025-08-21 17:16:17 +0530

    all working as expected

--- src/test/java/com/WynkMusic/Runner/TestRunner.java
+++ src/test/java/com/WynkMusic/Runner/TestRunner.java
@@ -1,16 +1,14 @@
 package com.WynkMusic.Runner;
 
-import org.junit.runner.RunWith;
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
 
-import io.cucumber.junit.Cucumber;
-import io.cucumber.junit.CucumberOptions;
-
-@RunWith(Cucumber.class)
+	
 @CucumberOptions(features = "src/test/resources/features", // Specify the path to your feature files
 		glue = { "com.WynkMusic.StepDefintions" }, // Specify the package where your step definitions are located
 		tags = "@WynkMusicTest", // Specify the tags if you want to run only specific scenarios
 		plugin = { "pretty", "html:target/cucumber-reports" } // Specify the desired output formats
 )
-public class TestRunner {
+public class TestRunner extends AbstractTestNGCucumberTests  {
 	// This class will be empty
 }
